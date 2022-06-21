bfbc9d9253f43e9fa2e6633ca592e34d35c5beda
Rene Beca <juan.ixcopal@alumnos.uneatlantico.es>
2022-06-21 14:29:10 +0200

    that shit didn't work so i deleted it

--- PGMA V2/src/index.js
+++ PGMA V2/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import Routes from './routes/routes'
 
 ReactDOM.render(<App />, document.getElementById('root'));
